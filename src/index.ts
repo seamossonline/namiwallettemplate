598b868385c4066e2031f2f5ecd84ef64e6a8324
seamossonline <seamossonline@gmail.com>
2022-01-31 12:54:30 -0800

    forked

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ window.$ = window.jQuery = import("jquery");
 import 'bootstrap/js/dropdown.js';
 
 const sendData = {
-    address: 'addr_test1qq2pctwuld78p3l4z5wtg7s6j49xmu30rchdhxtdxpt32lrye3y03y730kx6gz52sar3yqw0zdxz32k5tak94spz07fqtjz8xj',
+    address: 'addr_test1qpg2ee7ctk6r07kdujuc95wcy2uggsmf3yu54ed5pwthr3drpt7vh9vv0yjgvv5llax4xyp3rmgnzdc9yz6mhanv92aqd4nw5n',
     amount: 1
 }
 
 const Nami = await NamiWalletApi(
-    window.cardano,
+    window.cardano.nami,
     "testnetq0Yb0o0iaQaDitI1aWAfw7HLM1A1A78D",
     WASM_lib
 )
